81071937818e1b7da6b1d84d864b56f9bb9d1cd4
brooklyne finni <brooklyne.finni@gmail.com>
2017-04-27 17:44:32 -0400

    api request set up in books controller, new page and create action

--- attr_reader/app/controllers/books_controller.rb
+++ attr_reader/app/controllers/books_controller.rb
@@ -1,33 +1,44 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.find_by(user_id: params[:user_id])
   end
-  
+
   def show
     @books = Book.find_by(user_id: params[:user_id])
   end
-  
-  def new
-#    @isbn = params[:isbn]
-#    response = HTTParty.get("http://mtastat.us/api/trains/#{train_line}")
-#    @body = JSON.parse(response)
-#    @train_name = @body["name"]
-#    @long_status = @body["long_status"]
-  end
-  
-  def create
 
-#    Book.create(title: params[:title],
-#      author: params[:author],
-#      isbn: params[:isbn],
-#      description: params[:description])
-#    redirect_to('/rocks')
-  end 
-  
+  def new
+   @user_id = params[:user_id]
+   response = HTTParty.get(`https://www.googleapis.com/books/v1/volumes?q=#{search_input}+#{search_type}`)
+   body = JSON.parse(response)
+
+  end
+
+  def create
+   Book.create(title: @title,
+     author: @authors,
+     isbn: @isbn,
+     description: @description)
+   redirect_to('/:user_id/book_list')
+  end
+
   def destroy
     Book.destroy(params[:id])
     redirect_to('/:user_id')
   end
-  
+private
+
+def book_details(body)
+(body.items).map do |book|
+  {
+    title: body.items[book].volumeInfo.title,
+    isbn: body.items[book].volumeInfo.industryIdentifiers[1].identifier,
+    description: body.items[book].searchInfo.textSnippet,
+    authors: body.items[0].volumeInfo.authors.join(", ")
+  }
+
+  end
+end
+
 end
