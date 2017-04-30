ec17553a8513826470688c5cc92213b13c1e1496
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-04-30 12:32:45 -0400

    added signout feature to users/sign_out in line 7 routes.rb

--- attr_reader/config/routes.rb
+++ attr_reader/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "/login", to: "devise/sessions#new"
+    get "/users/sign_out", to: "devise/sessions#new"
+
   end
 
   get "/", to: "home#index"
