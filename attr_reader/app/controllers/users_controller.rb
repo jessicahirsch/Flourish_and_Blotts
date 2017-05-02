5bf9f0e000b9402a44facc065c8cf79a6b723d65
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-05-02 16:48:52 -0400

    fixed delete account issue

--- attr_reader/app/controllers/users_controller.rb
+++ attr_reader/app/controllers/users_controller.rb
@@ -1,24 +1,10 @@
 class UsersController < ApplicationController
-
-
-#  def index
-#    @search = Book.search(params[:q])
-#    @books = @search.result
-#  end
-
-
-
-
 def show
 
     render "/devise/registrations/new"
 end
 
-# def created
-
-# end
-
-  def create
+def create
     user = User.create(username: params['user']['username'],
       email: params['user']['email'],
       password: params['user']['password']
